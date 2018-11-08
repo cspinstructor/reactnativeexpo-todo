57af79f1694261a6c9ef3db842589a0c4ec4fa85
student <student@hju>
2018-11-08 16:25:26 +0800

    Styles

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, Button, TouchableOpacity } from 'react-native';
 
 class App extends React.Component {
   state = {
@@ -21,46 +21,72 @@ class App extends React.Component {
   renderTodos = () => {
     return this.state.todo.map(t => {
       return (
-        <Text
-          key={t}
-          onPress={() => {
-            this.deleteTodo(t);
-          }}
-        >
-          {t}
-        </Text>
+        <TouchableOpacity key={t}>
+          <Text
+            style={styles.todo}
+            onPress={() => {
+              this.deleteTodo(t);
+            }}
+          >
+            {t}
+          </Text>
+        </TouchableOpacity>
       );
     });
   };
   render() {
     return (
-      <View style={styles.viewStyles}>
-        <Text>Hello world, this app is great</Text>
-        <TextInput
-          style={styles.inputStyles}
-          onChangeText={text => {
-            this.setState({ text });
-          }}
-          value={this.state.text}
-        />
-        <Button title="Add todo" color="green" onPress={this.addTodo} />
-        {this.renderTodos()}
+      <View style={styles.wholeStyle}>
+        <View style={styles.viewStyles}>
+          <Text style={styles.header}>Notes App</Text>
+          <TextInput
+            style={styles.inputStyles}
+            onChangeText={text => {
+              this.setState({ text });
+            }}
+            value={this.state.text}
+          />
+          <Button title="Add todo" color="#2286c3" onPress={this.addTodo} />
+        </View>
+        <View style={{ marginTop: 100 }} />
+        <View style={styles.outputarea}>{this.renderTodos()}</View>
       </View>
     );
   }
 }
 
 const styles = {
-  viewStyles: {
+  wholeStyle: {
     flex: 1,
+    backgroundColor: '#E1E2E1'
+  },
+  viewStyles: {
+    marginTop: 30,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    backgroundColor: '#64b5f6',
+    padding: 10
   },
   inputStyles: {
     height: 40,
     width: 300,
-    borderColor: 'green',
-    borderWidth: 1
+    borderColor: 'black',
+    borderWidth: 1,
+    margin: 10
+  },
+  header: {
+    fontSize: 30,
+    color: 'black',
+    fontWeight: 'bold'
+  },
+  outputarea: {
+    backgroundColor: '#E1E2E1',
+    alignItems: 'center',
+    padding: 10,
+    fontSize: 15
+  },
+  todo: {
+    fontSize: 18
   }
 };
 
